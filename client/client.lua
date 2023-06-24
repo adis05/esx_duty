aa0f698557b699248eda83453bf389f030722ea2
AfterDutySleep <102389489+adis05@users.noreply.github.com>
2023-06-25 00:36:02 +0700

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -10,7 +10,7 @@ CreateThread(function()
                         {
                             name = i,
                             icon = 'fa-solid fa-business-time',
-                            label = '[' .. i .. '] ' .. 'Clock in/out!',
+                            label = '[' .. i .. '] ' .. 'Duty On/Off!',
                             canInteract = function(entity, distance, coords, name)
                                 local playerData = ESX.GetPlayerData()
                                 local jobTable = Config.Jobs[name]
@@ -30,23 +30,23 @@ CreateThread(function()
                                 if jobName == jobTable.ClockedInJob then
                                     ESX.TriggerServerCallback("joblock:ToggleClock", function(success, totalTime)
                                         if not success then
-                                            return ESX.ShowNotification("Couldn't clock you out...")
+                                            return ESX.ShowNotification("Couldn't Duty you Off...")
                                         end
 
                                         local notificationText
                                         if totalTime ~= nil then
-                                            notificationText = string.format("You are clocked off!\nTotal Menit: %d", totalTime)
+                                            notificationText = string.format("You are Duty off!\nTotal Menit: %d", totalTime)
                                         else
-                                            notificationText = "You are clocked off!"
+                                            notificationText = "You are Off Duty!"
                                         end
 
                                         return ESX.ShowNotification(notificationText)
                                     end, data.name, false)
                                 elseif jobName == jobTable.ClockedOutJob then
                                     ESX.TriggerServerCallback("joblock:ToggleClock", function(success)
-                                        if not success then return ESX.ShowNotification("Couldn't clock you in...") end
+                                        if not success then return ESX.ShowNotification("Couldn't Duty you On...") end
 
-                                        return ESX.ShowNotification("You are clocked in!")
+                                        return ESX.ShowNotification("You are On Duty!")
                                     end, data.name, true)
                                 end
                             end
